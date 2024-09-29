a70fe619d32e6477fba1f505fec4d7d49d23869d
Mustafa Celik <sb.besiktas@gmail.com>
2024-09-29 22:37:54 +0300

    Add project files.

---
+++ CarBook.Persistance/Repositories/CarRepositories/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistance.Repositories.CarRepository
+{
+    public class CarRepository: ICarRepository
+    {
+        private readonly CarBookContext _context;
+        public List<Car> GetCarsListWithBrands()
+        {
+           var values=_context.Cars.Include(x=>x.Brand).ToList();
+            return values;
+        }
+    }
+}
---
+++ Presentation/CarBook.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,51 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FeaturesController(IMediator mediator)
+        {
+            _mediator=mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FeatureList()
+        {
+            var values = await _mediator.Send(new GetFeatureQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeature(int id)
+        {
+            var value= _mediator.Send(new GetFeatureByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Feature Added");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFeature(int id)
+        {
+            await _mediator.Send(new RemoveFeatureCommand(id));
+            return Ok("Feature Deleted");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Feature Updated");
+        }
+    }
+}
